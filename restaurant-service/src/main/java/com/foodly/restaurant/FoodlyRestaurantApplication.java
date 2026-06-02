b8875aa844892a84731612549d2eac20e8159d2b
mbsajay <mbsajay@gmail.com>
2026-06-02 20:28:39 +0530

    restaurant-service: add maven module, application class and bootstrap config

---
+++ restaurant-service/src/main/java/com/foodly/restaurant/FoodlyRestaurantApplication.java
@@ -0,0 +1,12 @@
+package com.foodly.restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodlyRestaurantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodlyRestaurantApplication.class, args);
+    }
+}
