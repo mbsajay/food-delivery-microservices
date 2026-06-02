7e0313cae434acd2dfc02edaf3333c0754b9e65c
mbsajay <mbsajay@gmail.com>
2026-06-02 20:38:15 +0530

    delivery-service: add maven module, application class and bootstrap config

---
+++ delivery-service/src/main/java/com/foodly/delivery/FoodlyDeliveryApplication.java
@@ -0,0 +1,12 @@
+package com.foodly.delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodlyDeliveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodlyDeliveryApplication.class, args);
+    }
+}
