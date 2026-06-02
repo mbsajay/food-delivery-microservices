337c20823b163a4267089e814676a1ebda779b5d
mbsajay <mbsajay@gmail.com>
2026-06-02 20:32:04 +0530

    order-service: add maven module, application class and bootstrap config

---
+++ order-service/src/main/java/com/foodly/order/FoodlyOrderApplication.java
@@ -0,0 +1,12 @@
+package com.foodly.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodlyOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodlyOrderApplication.class, args);
+    }
+}
