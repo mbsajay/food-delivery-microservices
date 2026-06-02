b45b3567897fa77d164e76d7e0d8d98b79f5515a
mbsajay <mbsajay@gmail.com>
2026-06-02 19:47:43 +0530

    api-gateway: add Spring Boot application class

---
+++ api-gateway/src/main/java/com/foodly/gateway/FoodlyGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.foodly.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodlyGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodlyGatewayApplication.class, args);
+    }
+}
